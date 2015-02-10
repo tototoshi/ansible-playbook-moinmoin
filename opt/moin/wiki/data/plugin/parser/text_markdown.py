909d34031af434b819c39f58c2ce94f42b9b5629
toshi <toshi@pixiv.com>
2015-02-10 20:04:40 +0900

    Some useful markdown extensions

--- opt/moin/wiki/data/plugin/parser/text_markdown.py
+++ opt/moin/wiki/data/plugin/parser/text_markdown.py
@@ -17,6 +17,11 @@ class Parser:
                 'markdown.extensions.fenced_code',
                 'markdown.extensions.toc',
                 'markdown.extensions.codehilite',
+                'markdown.extensions.wikilinks',
+                'markdown.extensions.tables',
+                'markdown.extensions.def_list',
+                'markdown.extensions.nl2br',
+                'markdown.extensions.headerid',
                 ])
         try:
             self.request.write(formatter.rawHTML(output_html))
